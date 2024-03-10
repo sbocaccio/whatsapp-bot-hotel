bdeab11e62c2abe4966d8b435094aaecb1218fe3
sbocaccio <sebastian.bocaccio@tiendanube.com>
2024-03-10 11:08:06 -0300

    Intento devuelta

--- base-baileys-memory/install_dependencies.sh
+++ base-baileys-memory/install_dependencies.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # Download and install nvm
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+source ~/.bashrc
+
 
 # Check if nvm is installed successfully
 if command -v nvm &> /dev/null; then
