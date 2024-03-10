cc31b7b950dd746ca7280f0708762e1f40fed6db
sbocaccio <sebastian.bocaccio@tiendanube.com>
2024-03-10 10:57:48 -0300

    Update archivo para isntalar dependencias

--- base-baileys-memory/install_dependencies.sh
+++ base-baileys-memory/install_dependencies.sh
@@ -1,3 +1,19 @@
-nvm install 16.14
-nvm use 16.14
-npm install
+#!/bin/bash
+
+# Download and install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+# Check if nvm is installed successfully
+if command -v nvm &> /dev/null; then
+    # Install Node.js version 16.14 using nvm
+    nvm install 16.14
+
+    # Use Node.js version 16.14
+    nvm use 16.14
+
+    # Install dependencies using npm
+    npm install
+else
+    echo "Error: nvm is not installed or not properly configured."
+    exit 1
+fi
