8ff829e42cd035bc403cc921ba116b559b732abf
sbocaccio <sebastian.bocaccio@tiendanube.com>
2024-03-23 15:58:35 -0300

    Agrego mas flujos separados

--- base-baileys-memory/app.js
+++ base-baileys-memory/app.js
@@ -9,35 +9,7 @@ import { flowNoEntendi } from './flows/flowNoEntendi.js';
 import {flowComplejos} from "./flows/flowComplejos.js";
 import {flowHabitaciones} from "./flows/flowHabitaciones.js";
 import {flowReservas} from "./flows/flowReservas.js";
-
-export const flowInstalaciones = addKeyword(['instalaciones', '3'])
-    .addAnswer(
-        [
-        '1. Complejo Cañuelas: ',
-        'Aquí tienes un detalle de las instalaciones del complejo de Cañuelas: Este complejo cuenta con un desayunador amplio, conectado con una cafetería y un kiosco 24hs. ',
-        'Las áreas de descanso que posee son las siguientes: ',
-        '- Pileta climatizada (Techada)',
-        '- Sauna seco',
-        '- Sala de masajes',
-        '- Parque',
-        '- Plaza blanda para niños'
-        ]
-    )
-    .addAnswer(
-    [
-        '2. Complejo San Miguel del Monte:',
-        'Aquí tienes un detalle de las instalaciones del complejo de San Miguel del Monte: Este complejo cuenta con un restorán, área de descanso y estacionamiento. ',
-        'Las áreas de descanso que posee son las siguientes: ',
-        '- Pileta climatizada (Techada)',
-        '- Parque amplio con reposeras y gazebos para descansar',
-        '- Plaza externa para niños',
-        '- Fogones para recrear en familia',
-    ]
-    )
-    .addAction(async (_, { gotoFlow }) => {
-        return gotoFlow(flowPrincipalSinBienvenida)
-    })
-
+import {flowInstalaciones} from "./flows/flowInstalaciones.js";
 
 const actionYaFueAtendido = async (ctx, { state, endFlow }) => {
     const myState = state.getMyState()
