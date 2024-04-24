8e10103405d0a1b4d9a5e7a2bb3e7d72d33beb55
sbocaccio <sebastian.bocaccio@tiendanube.com>
2024-04-24 15:08:55 -0300

    Add copy session

--- base-baileys-memory/copySession.sh
+++ base-baileys-memory/copySession.sh
@@ -4,6 +4,13 @@
 source_dir="saved_session"
 destination_dir="bot_sessions"
 
+
+if [ ! -d "$destination_dir" ]; then
+    # Create the folder if it doesn't exist
+    mkdir -p "$destination_dir"
+fi
+
+
 # Copy files from source to destination
 cp -r "$source_dir"/* "$destination_dir"/
 
