ec95f223b070d71074024a717eedbe81e7aa8f16
Sebastian bocaccio <sbocaccio@dc.uba.ar>
2024-08-07 20:57:11 -0300

    Log when a user sends a message

--- base-baileys-memory/actions/actionYaFueAtendido.js
+++ base-baileys-memory/actions/actionYaFueAtendido.js
@@ -1,6 +1,7 @@
 import {flowEmpty} from "../flows/flowEmpty.js";
 
 export const actionYaFueAtendido = async (ctx, { state, gotoFlow }) => {
+    console.log("New Message From:", ctx.from); // TODO: Create an "inform message" action that logs the user
     const flowCompletado = global.flowCompletionTrackerService.getPhoneCompletedFlows(ctx.from, state)
     if(flowCompletado) return gotoFlow(flowEmpty)
 }
