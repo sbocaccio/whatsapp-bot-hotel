499ec2a3d8879e72d9312f6364a8fd62be132e15
sbocaccio <98828814+sbocaccio@users.noreply.github.com>
2024-08-07 08:54:40 -0300

    Update flowNosComunicaremos.js

--- base-baileys-memory/flows/flowNosComunicaremos.js
+++ base-baileys-memory/flows/flowNosComunicaremos.js
@@ -2,7 +2,7 @@ import pkg from '@bot-whatsapp/bot';
 const { addKeyword } = pkg;
 
 export const flowNosComunicaremos = addKeyword(['2'])
-    .addAnswer("Nos estaremos comunicando personalmente en la brevedad para que puedas confirmar una reserva", null, async (ctx, { endFlow, state }) => {
+    .addAnswer("Por favor, comunicarse en horario de atención de 09:00hs a 20:00hs si desea realizar una reserva", null, async (ctx, { endFlow, state }) => {
         await global.flowCompletionTrackerService.setCompletedFlow(ctx.from, state)
         return endFlow();
     })
