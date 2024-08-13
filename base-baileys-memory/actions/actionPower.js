6aeaa117df46e587344d0af3573d850e092c3b37
Sebastian bocaccio <sbocaccio@dc.uba.ar>
2024-08-13 16:18:41 -0300

    Move User log to Power Action

--- base-baileys-memory/actions/actionPower.js
+++ base-baileys-memory/actions/actionPower.js
@@ -2,9 +2,11 @@ import {masterPhones} from '../constants/masterPhones.js';
 
 
 export const actionPower = async (ctx, { endFlow }) => {
+  console.log("New Message From:", ctx.from); // TODO: Create an "inform message" action that logs the user
 
   if (masterPhones.includes(ctx.from) && (ctx.body === 'PRENDER' || ctx.body === 'APAGAR')) {
     ctx.body === 'PRENDER' ? global.powerService.turnOn() : global.powerService.turnOff();
+
     const message = ctx.body === 'PRENDER' ? 'Bot encendido' : 'Bot apagado';
     return endFlow(message);
   }
