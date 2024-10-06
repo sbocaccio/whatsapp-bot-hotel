543e3ebd2661660f04efa6103a0ca1f4e56bf0d7
Sebastian bocaccio <sbocaccio@dc.uba.ar>
2024-10-06 13:47:53 -0300

    Log hour

--- base-baileys-memory/services/activeHoursChecker.service.js
+++ base-baileys-memory/services/activeHoursChecker.service.js
@@ -3,8 +3,8 @@ export class ActiveHoursChecker{
     isWithinActiveHours(startHour, endHour) {
         const currentDate = new Date();
         const currentHour = currentDate.getHours();
-        const currentMinutes = currentDate.getMinutes();
       
+        console.log("Current hour:", currentHour)
         // Check if the current hour is within the active range
         if (currentHour >= startHour && currentHour < endHour) {
           return true;
