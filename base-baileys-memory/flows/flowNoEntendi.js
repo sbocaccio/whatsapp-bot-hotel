56387fec4d3a7ce0851adf18118b4fdbce6cb7e6
Sebastian bocaccio <sbocaccio@dc.uba.ar>
2024-10-08 16:35:46 -0300

    Not continue conversation even when started outside of working hours

--- base-baileys-memory/flows/flowNoEntendi.js
+++ base-baileys-memory/flows/flowNoEntendi.js
@@ -1,9 +1,12 @@
 import pkg from '@bot-whatsapp/bot';
 import {actionPower} from "../actions/actionPower.js";
+import {actionActiveHours} from "../actions/actionActiveHours.js";
+
 const { addKeyword } = pkg;
 
 export const flowNoEntendi = addKeyword([''])
     .addAction(actionPower)
+    .addAction(actionActiveHours)
     .addAnswer('No pude entender tu respuesta. Por favor volvé a elegír.', null, (ctx, { fallBack }) => {
              return fallBack();
     }
--- base-baileys-memory/flows/flowNoEntendiInicial.js
+++ base-baileys-memory/flows/flowNoEntendiInicial.js
@@ -2,9 +2,12 @@ import pkg from '@bot-whatsapp/bot';
 const { addKeyword } = pkg;
 import { actionYaFueAtendido } from '../actions/actionYaFueAtendido.js';
 import {actionPower} from "../actions/actionPower.js";
+import {actionActiveHours} from "../actions/actionActiveHours.js";
+
 
 export const flowNoEntendiInicial = addKeyword([''])
     .addAction(actionPower)
+    .addAction(actionActiveHours)
     .addAction(actionYaFueAtendido)
     .addAnswer('Buenas! Para iniciar una conversación escribí "Hola"', null,async (_, { endFlow }) => {
         return endFlow();
