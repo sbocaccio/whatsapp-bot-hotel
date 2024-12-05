6f0e0e48ab0e5c07c2cc6c36974e606987b6c9ed
Sebastian bocaccio <sbocaccio@dc.uba.ar>
2024-12-05 11:14:30 -0300

    Updat images

--- base-baileys-memory/constants/images_path.js
+++ base-baileys-memory/constants/images_path.js
@@ -1,2 +1,5 @@
-export const CANUELAS_PATH_AVAILABILITY = 'https://github.com/sbocaccio/whatsapp-bot-hotel/blob/main/base-baileys-memory/images/precios_canuelas_abril.jpeg?raw=true'
-export const SAN_MIGUEL_PATH_AVAILABILITY = 'https://github.com/sbocaccio/whatsapp-bot-hotel/blob/main/base-baileys-memory/images/precios_san_miguel_abril.jpeg?raw=true'
+export const CANUELAS_PATH_NEW_AVAILABILITY = 'https://github.com/sbocaccio/whatsapp-bot-hotel/blob/main/base-baileys-memory/images/precios_canuelas_febrero.jpeg?raw=true'
+export const CANUELAS_PATH_OLD_AVAILABILITY = 'https://github.com/sbocaccio/whatsapp-bot-hotel/blob/main/base-baileys-memory/images/precios_canuelas_diciembre.jpeg?raw=true'
+
+export const SAN_MIGUEL_PATH_NEW_AVAILABILITY = 'https://github.com/sbocaccio/whatsapp-bot-hotel/blob/main/base-baileys-memory/images/precios_san_miguel_febrero.jpeg?raw=true'
+export const SAN_MIGUEL_PATH_OLD_AVAILABILITY = 'https://github.com/sbocaccio/whatsapp-bot-hotel/blob/main/base-baileys-memory/images/precios_san_miguel_febrero.jpeg?raw=true'
--- base-baileys-memory/flows/flowTarifas.js
+++ base-baileys-memory/flows/flowTarifas.js
@@ -1,12 +1,16 @@
 import pkg from '@bot-whatsapp/bot';
 import {
-    CANUELAS_PATH_AVAILABILITY,
-    SAN_MIGUEL_PATH_AVAILABILITY,
+    CANUELAS_PATH_NEW_AVAILABILITY,
+    CANUELAS_PATH_OLD_AVAILABILITY,
+    SAN_MIGUEL_PATH_OLD_AVAILABILITY,
+    SAN_MIGUEL_PATH_NEW_AVAILABILITY,
 } from "../constants/images_path.js";
 const { addKeyword} = pkg;
 
 export const flowTarifas = addKeyword(['1'])
     .addAnswer('Estamos trayendo la información. Esto puede tardar unos segundos...', null, null)
-    .addAnswer(' ', { media: CANUELAS_PATH_AVAILABILITY})
-    .addAnswer(' ', { media: SAN_MIGUEL_PATH_AVAILABILITY})
+    .addAnswer('Tarifas del 23/12 a febrero', { media: CANUELAS_PATH_NEW_AVAILABILITY})
+    .addAnswer('Tarifa hasta 22 de diciembre', { media: CANUELAS_PATH_OLD_AVAILABILITY})
+    .addAnswer('Tarifas del 23/12 a febrero', { media: SAN_MIGUEL_PATH_NEW_AVAILABILITY})
+    .addAnswer('Tarifa hasta 22 de diciembre', { media: SAN_MIGUEL_PATH_OLD_AVAILABILITY})
     .addAnswer("Por favor, comunicarse en horario de atención de 09:00hs a 20:00hs si desea realizar una reserva")
