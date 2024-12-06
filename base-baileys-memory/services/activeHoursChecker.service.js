fbe2f296846a3903b354a66d33a87eda0271577f
sbocaccio <98828814+sbocaccio@users.noreply.github.com>
2024-12-06 10:23:17 -0300

    Update activeHoursChecker.service.js

--- base-baileys-memory/services/activeHoursChecker.service.js
+++ base-baileys-memory/services/activeHoursChecker.service.js
@@ -1,10 +1,9 @@
 export class ActiveHoursChecker{
     
     isWithinActiveHours(startHour, endHour) {
-        const OFFSET = 3 
-        const currentDate = new Date();
-        var currentHour = currentDate.getHours();
-        currentHour = currentHour - OFFSET
+        const argentinaTime = new Date().toLocaleString("en-US", { timeZone: "America/Argentina/Buenos_Aires" });
+        const argentinaDate = new Date(argentinaTime);
+        var currentHour = argentinaDate.getHours()
       
         console.log("Current hour:", currentHour)
         // This only works if startHour > endHour (startHour = 21hs, endHour = 9hs)
