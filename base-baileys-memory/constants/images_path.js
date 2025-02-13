f01b0f2e9268236fa5e4e0d4a0ce46d20d965692
sbocaccio <98828814+sbocaccio@users.noreply.github.com>
2025-02-13 13:12:02 +0000

    Update images

--- base-baileys-memory/constants/images_path.js
+++ base-baileys-memory/constants/images_path.js
@@ -1,5 +1,2 @@
-export const CANUELAS_PATH_NEW_AVAILABILITY = 'https://github.com/sbocaccio/whatsapp-bot-hotel/blob/main/base-baileys-memory/images/precios_canuelas_febrero.jpeg?raw=true'
-export const CANUELAS_PATH_OLD_AVAILABILITY = 'https://github.com/sbocaccio/whatsapp-bot-hotel/blob/main/base-baileys-memory/images/precios_canuelas_diciembre.jpeg?raw=true'
-
-export const SAN_MIGUEL_PATH_NEW_AVAILABILITY = 'https://github.com/sbocaccio/whatsapp-bot-hotel/blob/main/base-baileys-memory/images/precios_san_miguel_febrero.jpeg?raw=true'
-export const SAN_MIGUEL_PATH_OLD_AVAILABILITY = 'https://github.com/sbocaccio/whatsapp-bot-hotel/blob/main/base-baileys-memory/images/precios_san_miguel_febrero.jpeg?raw=true'
+export const CANUELAS_PATH = 'https://github.com/sbocaccio/whatsapp-bot-hotel/blob/main/base-baileys-memory/images/precios_canuelas.jpeg?raw=true'
+export const SAN_MIGUEL_PATH = 'https://github.com/sbocaccio/whatsapp-bot-hotel/blob/main/base-baileys-memory/images/precios_san_miguel.jpeg?raw=true'
